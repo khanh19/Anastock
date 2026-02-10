1cf98afe3fc7ce38c0488086f84722e526fdc583
khanh19 <khanh02092000@gmail.com>
2026-02-10 17:59:22 -0500

    Creating project

---
+++ src/test/java/com/anastock/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anastock.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
